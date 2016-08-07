2292ecd959eea8aca86146609d4c514c203776f9
Chris Greene <archgoon@gmail.com>
2016-08-07 15:52:01 -0700

    Code cleanup.
    Setup takes the three core things, camera, scene, renderer,
    and wraps them into a 'world' object. Helpful 'render' method
    attached.

--- code.js
+++ code.js
@@ -1,39 +1,50 @@
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( 800, 640 );
-document.body.appendChild( renderer.domElement );
+var renderer;
+var scene;
+var camera;
 
-var scene = new THREE.Scene();
+function setup() {
+    // Renderer
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 800, 640 );
+    document.body.appendChild( renderer.domElement );
 
-var camera = new THREE.PerspectiveCamera(
-    35,         // Field of view
-    800 / 640,  // Aspect ratio
-    0.1,        // Near
-    10000       // Far
-);
-camera.position.set( 0, 0, 40 );
-camera.lookAt( scene.position );
+    // Scene
+    var scene = new THREE.Scene();
 
-var light = new THREE.PointLight( 0xFFFFFF );
-light.position.set( 0, 10, 0 );
-scene.add( light );
+    // Camera
+    var camera = new THREE.PerspectiveCamera(
+        35,         // Field of view
+        800 / 640,  // Aspect ratio
+        0.1,        // Near
+        10000       // Far
+    );
+    camera.name = "Camera";
+    camera.position.set( 0, 0, 40 );
+    camera.lookAt( scene.position );
 
-var ambient = new THREE.AmbientLight(0x404040);
-scene.add( ambient );
+    // Point light
+    var light = new THREE.PointLight( 0xFFFFFF );
+    light.name = "PointLight";
+    light.position.set( 0, 10, 0 );
+    scene.add( light );
 
-var geometry = new THREE.SphereGeometry( 5, 32, 32);
-var material = new THREE.MeshPhongMaterial( {vertexColors: THREE.FaceColors} );
-var sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
+    // Ambient
+    var ambient = new THREE.AmbientLight(0x404040);
+    ambient.name = "Ambient";
+    scene.add( ambient );
 
-for (var vert of sphere.geometry.vertices) {
-//  console.log(JSON.stringify(vert));
+    // Sphere
+    var geometry = new THREE.SphereGeometry( 5, 32, 32);
+    var material = new THREE.MeshPhongMaterial( {vertexColors: THREE.FaceColors} );
+    var sphere = new THREE.Mesh( geometry, material );
+    sphere.name = "Sphere";
+    scene.add( sphere );
+
+    for (var face of sphere.geometry.faces) {
+      face.color.setRGB(Math.random(),Math.random(),Math.random());
+    }
+    return { camera, renderer, scene, render: (...a) =>renderer.render(...a) };
 }
-
-for (var face of sphere.geometry.faces) {
-  face.color.setRGB(Math.random(),Math.random(),Math.random());
-}
-for (var face of sphere.geometry.faces) {
-  console.log(JSON.stringify(face));
-}
-geometry.elementsNeedUpdate = true;
-renderer.render(scene, camera);
+var world = setup();
+console.log(world);
+world.render(world.scene, world.camera);
